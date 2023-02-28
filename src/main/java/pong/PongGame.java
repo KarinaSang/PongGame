08de24dfcab5a8ee4514db2d83b62267488d051c
unknown <ycsang@uwaterloo.ca>
2023-02-28 18:05:55 -0500

    added initsettings

--- src/main/java/pong/PongGame.java
+++ src/main/java/pong/PongGame.java
@@ -1,7 +1,19 @@
 package pong;
 
 import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
 
 public class PongGame extends GameApplication {
 
+    @Override
+    protected void initSettings(GameSettings settings) {
+        settings.setWidth(800);
+        settings.setHeight(800);
+        settings.setTitle("PongGame");
+        settings.setVersion("1.0");
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
